ff9a801899c3274a24ca28b9d5f79f3da17d56fe
MatthiasMarczyszyn <MatthiasMarczyszyn@github.com>
2021-11-23 03:27:03 +0100

    ugabuga v3

--- Python_Script/example.py
+++ Python_Script/example.py
@@ -1,33 +1,29 @@
 import numpy as np
 import cv2
 import pickle
+import urllib.request
 import matplotlib.pyplot as plt
 
-width = 640
-height = 480
 
-cap = cv2.VideoCapture(0)
-cap.set(3,width)
-cap.set(4,height)
 
 pickle_in=open("model_trained.p","rb")
 model = pickle.load(pickle_in)
-
+url = 'http://192.168.1.67/cam-lo.jpg'
 def image_processing(img):
+    img = cv2.flip(img,-1)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.equalizeHist(img)
     img = img/255
     return img
 
 while True:
-    success, imgOrg = cap.read()
-    img = np.asarray(imgOrg)
+    cap = urllib.request.urlopen(url)
+    imgNp = np.array(bytearray(cap.read()),dtype=np.uint8)
+    img=cv2.imdecode(imgNp,-1)
     img = cv2.resize(img,(32,32))
     img = image_processing(img)
     img = img.reshape(1,32,32,1)
     classIndex = (model.predict(img))
     result = np.argmax(classIndex)
     proba = np.amax(classIndex)
-    if proba >0.95:
-        print(result
-              )
+    print(result,proba)
--- Python_Script/main.py
+++ Python_Script/main.py
@@ -1,5 +1,20 @@
 import socket
 from time import sleep
+import numpy as np
+import cv2
+import pickle
+import urllib.request
+import matplotlib.pyplot as plt
+
+pickle_in=open("model_trained.p","rb")
+model = pickle.load(pickle_in)
+url = 'http://192.168.1.67/cam-lo.jpg'
+def image_processing(img):
+    img = cv2.flip(img,-1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.equalizeHist(img)
+    img = img/255
+    return img
 
 host = "192.168.1.67"
 port = 23
@@ -8,7 +23,16 @@ s.connect((host, port))
 
 x = 0
 while x != "END": # TODO głupia małpo zamien na for
-    x = input()
-    s.sendall(bytes(str(x), 'utf-8'))
+    cap = urllib.request.urlopen(url)
+    imgNp = np.array(bytearray(cap.read()),dtype=np.uint8)
+    img=cv2.imdecode(imgNp,-1)
+    img = cv2.resize(img,(32,32))
+    img = image_processing(img)
+    img = img.reshape(1,32,32,1)
+    classIndex = (model.predict(img))
+    result = np.argmax(classIndex)
+    proba = np.amax(classIndex)
+    print(result,proba)
+    s.sendall(bytes(str(result), 'utf-8'))
     sleep(1)
 s.close()
